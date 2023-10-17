e93ccfa4a4cb6be6d4b55596baa6ad2ca0b7e3bb
Jason1420 <hai.truong1420@gmail.com>
2023-10-17 13:13:34 +0700

    change logic logout

--- src/main/java/com/store/cosmetic/jwt/JwtConstant.java
+++ src/main/java/com/store/cosmetic/jwt/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.store.cosmetic.jwt;
 
 public class JwtConstant {
-    public static final long JWT_EXPIRATION = 60 * 1000 * 60; // 60m
+    public static final long JWT_EXPIRATION = 60 * 1000 * 60 * 3; // 60m
     public static final long DELETE_ACCESS_TOKEN_TIME = 0; // 20s
     public static final long REFRESH_TOKEN_EXPIRATION = 60 * 1000 * 60 * 24;
     public static final String JWT_SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
